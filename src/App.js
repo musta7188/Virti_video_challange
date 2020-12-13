5fe6b1f721888ab63f13c0cd4973dc8128153d9d
musta7188 <musta7188@hotmail.it>
2020-12-13 13:12:51 +0000

    video animation moved to the animation component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Video from "./video/Video";
 import { connect } from "react-redux";
 import Image1 from "./images/Image1";
 import Image2 from "./images/Image2";
 import Image3 from "./images/Image3";
 
-import { useSpring, animated } from "react-spring";
 
 function App({ videoPlay }) {
-  const props = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-  });
+  
 
   return (
     <>
@@ -26,10 +22,8 @@ function App({ videoPlay }) {
         <Image3 />
         <Image2 />
         <Image1 />
-
-        <animated.div style={props}>
           <Video />
-        </animated.div>
+     
       </div>
     </>
   );
--- src/video/Video.js
+++ src/video/Video.js
@@ -1,44 +1,51 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import video from "./Big_Buck_Bunny_1080_10s_5MB.mp4";
 import { connect } from "react-redux";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
-
-function Video({setPlaying, stopPlaying, setCurrentTimeVideo}) {
-
+import { useSpring, animated } from "react-spring";
+function Video({ setPlaying, stopPlaying, setCurrentTimeVideo }) {
+  ////animation with transition opacity
+  const props = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
 
   const handle = useFullScreenHandle();
- 
-  const handelProgress = (element) =>{
-      const time = element.nativeEvent.path[0].currentTime
-      setCurrentTimeVideo(time)
 
-  }
-
-  
+  const handelProgress = (element) => {
+    const time = element.nativeEvent.path[0].currentTime;
+    setCurrentTimeVideo(time);
+  };
 
   return (
     <>
-    <FullScreen handle={handle}>
-      <video src={video}
-        controls
-              onPlay={() => setPlaying()}
-              onEnded={() => stopPlaying()}
-              onTimeUpdate={handelProgress}  
-              
+      <FullScreen handle={handle}>
+        <animated.div style={props}>
+          <video
+            src={video}
+            controls
+            onPlay={() => setPlaying()}
+            onEnded={() => stopPlaying()}
+            onTimeUpdate={handelProgress}
           />
-    </FullScreen>
-     
-          <button onClick={handle.enter} className="button"> Fullscreen mode</button>
+        </animated.div>
+        <button onClick={handle.enter} className="button">
+        {" "}
+        Fullscreen mode
+      </button>
+      </FullScreen>
 
-      </>
+     
+    </>
   );
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    setPlaying: () => dispatch({ type: "VIDEO_PLAY"}),
-    stopPlaying: () => dispatch({ type: "VIDEO_STOP"}),
-    setCurrentTimeVideo: (currentState) => dispatch({ type: "VIDEO_TIMESTAMP", payload: {currentState}}),
+    setPlaying: () => dispatch({ type: "VIDEO_PLAY" }),
+    stopPlaying: () => dispatch({ type: "VIDEO_STOP" }),
+    setCurrentTimeVideo: (currentState) =>
+      dispatch({ type: "VIDEO_TIMESTAMP", payload: { currentState } }),
   };
 };
 
